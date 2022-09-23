5cc7b931314bcb2d87afdfb6ce39b6b636bde0f9
Gideonites <gideonsaved@gmail.com>
2022-09-23 23:14:32 +0000

    updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,53 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
  * Return: the pointer to dest.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+char *_strncpy(char *dest, char *src, int n)
 {
-		int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int i;
 
-			while (*(n1 + c1) != '\0')
-						c1++;
-				while (*(n2 + c2) != '\0')
-							c2++;
-					if (c1 >= c2)
-								bg = c1;
-						else
-									bg = c2;
-							if (size_r <= bg + 1)
-										return (0);
-								r[bg + 1] = '\0';
-									c1--, c2--, size_r--;
-										dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-											while (bg >= 0)
-													{
-																op = dr1 + dr2 + add;
-																		if (op >= 10)
-																						add = op / 10;
-																				else
-																								add = 0;
-																						if (op > 0)
-																									*(r + bg) = (op % 10) + 48;
-																								else
-																												*(r + bg) = '0';
-																										if (c1 > 0)
-																														c1--, dr1 = *(n1 + c1) - 48;
-																												else
-																																dr1 = 0;
-																														if (c2 > 0)
-																																		c2--, dr2 = *(n2 + c2) - 48;
-																																else
-																																				dr2 = 0;
-																																		bg--, size_r--;
-																																			}
-												if (*(r) == '0')
-															return (r + 1);
-													else
-																return (r);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
