54e0415fffd4e757486ac4e743ad1289ac672828
Your Name <you@example.com>
2022-09-23 05:36:52 -0700

    print 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include <stdio.h>"
+#include <stdio.h>
 /**
  * print_to_98 - Prints all natural numbers
  * from n to 98
@@ -9,16 +9,20 @@
  */
 void print_to_98(int n)
 {
-	for (; n > 98; n--)
+	if (n > 98)
 	{
-		printf("%d, ", n);
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
 	}
-	else
+	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
-			printf("%d\n", n);
+			printf("%d, ", n);
 		}
 	}
+	printf("%d\n", n);
 }
 
